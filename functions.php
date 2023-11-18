39c1a17c8e707773656ede21ccd742613fff3bcd
Sergey Bodnaryuk <s.bodnaryuk@gmail.com>
2023-11-18 20:39:47 +1000

    Добавляет функцию-шаблонизатор

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+// Шаблонизатор
+function include_template($path, $data)
+{
+	if (file_exists($path)) {
+		extract($data);
+
+		ob_start();
+
+		include($path);
+
+		return ob_get_clean();
+	} else {
+		return '';
+	}
+}
+
+// Форматирует цену
+function formatPrice($price)
+{
+	$price = ceil($price);
+
+	if ($price >= 1000) {
+		$price = number_format($price, 0, '.', ' ');
+	}
+
+	return $price . ' ₽';
+}
